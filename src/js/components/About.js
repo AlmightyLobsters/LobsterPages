47a6dc384abcd3a58691d4718305eadbdfa71df0
VojtechStep <vojtechstepancik@outlook.com>
2016-12-14 21:14:20 +0100

    Updated ESLint config, to finish later

--- src/js/components/About.js
+++ src/js/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 require("../../scss/About.scss");
 
 export const About = _ => (
-    <main className="about">
-        <h3>Who we are</h3>
-    </main>
+	<main className="about">
+		<h3>Who we are</h3>
+	</main>
 );
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import { LobsterRoutes } from './routes';
+import {LobsterRoutes } from './routes';
 
 const renderPage = () =>
 	render(
