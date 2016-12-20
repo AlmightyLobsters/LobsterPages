1dd58a61f4d50488f301ecf5264145d217754019
James Bohacek <jamesbohacek@outlook.com>
2016-12-20 12:28:24 +0100

    further development of design elements

--- src/js/components/Articles.js
+++ src/js/components/Articles.js
@@ -10,5 +10,11 @@ export const Articles = _ => (
 				<h2>A <b>sassy</b> subtitle</h2>
 			</header>
 		</section>
+		<section className="articles_wrapper">
+			<h3>We launched a site</h3>
+			<article>
+				After a month full of planning and development we finally managed to launch a site of our own. Happy New Year!
+			</article>
+		</section>
 	</div>
 )
