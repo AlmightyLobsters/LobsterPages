8251de25abd8ac7afa6d1d5da8398312ade50266
VojtechStep <vojtechstepancik@outlook.com>
2016-12-30 20:49:22 +0100

    Back on rect-router v3 and migrating on TravisCI

---
+++ src/js/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+require('../../scss/NotFound.scss');
+
+export const NotFound = _ => (
+	<div id="notFound">
+		<h1>Sorry, the page you requested doesn't exist</h1>
+	</div>
+);
