dda62cb1a86af8f8ed21c95f16cf420126762f2c
James Bohacek <jamesbohacek@outlook.com>
2017-01-01 04:32:05 +0100

    footer done

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -4,6 +4,33 @@ require("../../scss/Footer.scss");
 
 export const Footer = _ => (
 	<div id="footer">
-		&copy; Almighty Lobsters
+		<section className="row">
+			<section id="socialNetworks" className="col-1-3">
+				<h6>Humři na sociálních sítích</h6>
+				<ul>
+					<li><a href="https://www.facebook.com/ALCanSat" target="_blank">Facebook</a></li>
+					<li><a href="https://twitter.com/almghtLobsters" target="_blank">Twitter</a></li>
+					<li><a href="https://www.youtube.com/channel/UCox2-N19JXhrZlNf8cEM2Zw" target="_blank">YouTube</a></li>
+					<li><a href="https://myaccount.google.com/brandaccounts/102005371252944081919/view?pli=1" target="_blank">GooglePlus</a></li>
+				</ul>
+			</section>
+			<section id="CanSat" className="col-1-3">
+				<h6>Více o CanSatu</h6>
+				<ul>
+					<li><a href="http://esero.scientica.cz/cansat" target="_blank">Český CanSat</a></li>
+					<li><a href="http://www.esa.int/Education/CanSat" target="_blank">ESA</a></li>
+				</ul>
+			</section>
+			<section id="github" className="col-1-3">
+				<h6>GitHub</h6>
+				<ul>
+					<li><a href="https://github.com/AlmightyLobsters/LobsterPages" target="_blank">Tato stránka</a></li>
+					<li><a href="https://github.com/AlmightyLobsters/CanSat2017.Probe" target="_blank">Firmware do sondy</a></li>
+				</ul>
+			</section>
+		</section>
+		<section id="copy">
+			<h6>&copy; 2017 Almighty Lobsters</h6>
+		</section>
 	</div>
 );
