346e6fbff3bd0e92b057940d56159ae4b8aafc76
VojtechStep <vojtechstepancik@outlook.com>
2017-01-08 20:02:46 +0100

    Sitemap is generated on build

--- build.js
+++ build.js
@@ -1,5 +1,6 @@
 /* eslint no-console: 0 */
 const join = require('path').join;
+const fs = require('fs');
 
 // SASS
 require('node-sass').render(
@@ -7,17 +8,19 @@ require('node-sass').render(
         outputStyle: "compressed"
     }, function(err, result) {
     if(err) console.error(err);
-    else
+    else {
+        if(!fs.existsSync(join(__dirname, 'public', 'styles'))) fs.mkdirSync(join(__dirname, 'public', 'styles'));
         require('postcss')([require('autoprefixer'), require('postcss-url')])
             .process(result.css, {
                 from: join(__dirname, 'src', 'scss', 'main.scss'),
                 to: join(__dirname, 'public', 'styles', 'main.css')
             }).then(function(result) {
-                require('fs').writeFile(join(__dirname, 'public', 'styles', 'main.css'), result.css, function(err) {
+                fs.writeFile(join(__dirname, 'public', 'styles', 'main.css'), result.css, function(err) {
                     if (err) console.error(err);
                     else console.log('Style bundled');
                 });
             });
+    }
 });
 
 // JS
@@ -25,3 +28,10 @@ require('webpack')(require('./webpack.config')).run(function(err, stats) {
     if (err) console.error(err);
     else console.log('Client bundled');
 });
+
+// Sitemap
+require('babel-register');
+new (require('react-router-sitemap').default)(require('./src/js/routes').LobsterRoutes)
+    .build('http://almighty.lobsters.tech')
+    .save('./public/sitemap.xml');
+console.log('Sitemap generated');
