1e7a5f792c7e683bc51fc17d5768410e758125ab
VojtechStep <vojtechstepancik@outlook.com>
2017-01-22 17:45:43 +0100

    Article without image made possible

--- src/js/components/Article.js
+++ src/js/components/Article.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 export const Article = ({data: { title, text, imgFormat, reversed, imgPath}}) => (
     <section className="container" style={ reversed ? {flexDirection: 'row-reverse'} : {}}>
-        <article className={`col-${imgFormat === 'tall' ? 2 : 1}-${imgFormat === 'medium' ? 2 : 3}`}>
+        <article className={`col-${imgFormat === 'tall' ? 2 : 1}-${!imgPath ? 1 : (imgFormat === 'medium' ? 2 : 3)}`}>
             <div className="title"><h3>{title}</h3></div>
             <div className="article_innerWrapper">
                 {(typeof(text) === 'string' ? [text] : text).map(par => <p dangerouslySetInnerHTML={{__html: par}} />)}
             </div>
         </article>
-        <a href={imgPath} className={`col-${imgFormat === 'wide' ? 2 : 1}-${imgFormat === 'medium' ? 2 : 3}`}><img src={imgPath} alt={title} /></a>
+        { !!imgPath &&
+            <a href={imgPath} className={`col-${imgFormat === 'wide' ? 2 : 1}-${imgFormat === 'medium' ? 2 : 3}`}><img src={imgPath} alt={title} /></a>
+        }
     </section>
 );
