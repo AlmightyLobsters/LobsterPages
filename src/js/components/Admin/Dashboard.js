fc4723ce7d933bc78d1c4f41d16ebc1791fab265
VojtechStep <vojtechstepancik@outlook.com>
2017-01-23 21:02:17 +0100

    Edit and add articles UI

--- src/js/components/Admin/Dashboard.js
+++ src/js/components/Admin/Dashboard.js
@@ -7,8 +7,14 @@ export class Dashboard extends React.Component {
         super(props);
         this.state = {
             err: null,
-            articles: []
+            articles: [],
+            adding: false,
+            newArt: {
+                title: '',
+                text: ''
+            }
         };
+        this.articleElements = [];
     }
 
     componentDidMount() {
@@ -28,21 +34,39 @@ export class Dashboard extends React.Component {
     }
 
     deleteArticle(id) {
-        return e => {
-            e.preventDefault();
-            const select = this.state.articles.find(val => val.id===id);
-            if (confirm(`Do you want to delete article: '${select.title}'?`)) // eslint no-alert:0
-                axios.delete(`/articles/${id}`)
-                    .then(res => {
-                        this.setState((prevState, props) => ({
-                            err: null,
-                            articles: prevState.articles.filter(a => a.id !== res.data.id)
-                        }));
-                    })
-                    .catch(err => {
-                        this.setState({err});
-                    });
-        };
+        const select = this.state.articles.find(val => val.id===id);
+        if (confirm(`Do you want to delete article: '${select.title}'?`)) // eslint no-alert:0
+            axios.delete(`/articles/${id}`)
+                .then(res => {
+                    this.setState((prevState, props) => ({
+                        err: null,
+                        articles: prevState.articles.filter(a => a.id !== res.data.id)
+                    }));
+                })
+                .catch(err => {
+                    this.setState({err});
+                });
+    }
+
+    toggleActive(id) {
+        const isOn = this.articleElements[id].className.match(/(?:^|\s)active(?!\S)/g);
+        this.articleElements.forEach(item => item.className = item.className.replace(/(?:^|\s)active(?!\S)/g, ''));
+        if (!isOn) this.articleElements[id].className += ' active';
+    }
+
+    toggleAddView() {
+        this.setState(prevState => ({
+            adding: !prevState.adding
+        }));
+    }
+
+    updateTitle(prop, val) {
+        this.setState(prevState => ({
+            newArt: {
+                ...prevState.newArt,
+                [prop]: val
+            }
+        }));
     }
 
     render() {
@@ -51,19 +75,70 @@ export class Dashboard extends React.Component {
                 <h3>Dashboard</h3>
                 <main>
                     <div className="articleList">
-                        <button>Add an article</button>
+                        {this.state.adding
+                            ? (
+                                <div className="articleHeader active">
+                                    <div className="editForm">
+                                        <input name="title" className={`${this.state.newArt.title === '' ? 'invalid' : ''}`} onChange={e => this.updateTitle('title', e.target.value)} type="text" />
+                                        <textarea name="text" className={`${this.state.newArt.text === '' ? 'invalid' : ''}`} onChange={e => this.updateTitle('text', e.target.value)} />
+                                        <section>
+                                            <section>
+                                                <section>
+                                                    <label htmlFor="imgFormat">Image Format: </label>
+                                                    <select id="imgFormat">
+                                                        <option value="wide">Wide</option>
+                                                        <option value="medium">Medium</option>
+                                                        <option value="tall">Tall</option>
+                                                    </select>
+                                                </section>
+                                                <section>
+                                                    <label htmlFor="reversed">Image on the left</label>
+                                                    <input type="checkbox" id="reversed"/>
+                                                </section>
+                                            </section>
+                                            <section className="imgSelect">
+                                                <input type="file" name="headerImg"/>
+                                            </section>
+                                            <section className="buttons">
+                                                <button disabled={this.state.newArt.text === '' || this.state.newArt.title === ''}>Add</button>
+                                                <button onClick={e => this.toggleAddView()}>Cancel</button>
+                                            </section>
+                                        </section>
+                                    </div>
+                                </div>
+                            )
+                            : <button onClick={e => this.toggleAddView()}>Add an article</button>
+                        }
                         {this.state.err
                             ? <p>{this.state.err}</p>
                             : this.state.articles.map(art => (
-                                <div className="articleHeader">
+                                <div className="articleHeader" ref={c => this.articleElements[art.id] = c}>
                                     <header>
                                         <p>{art.title}<span className="idIndicator">{`#${art.id}`}</span></p>
                                         <div className="controls">
-                                            <button className="edit">Edit</button>
-                                            <button className="delete" onClick={this.deleteArticle(art.id)}>Delete</button>
+                                            <button className="edit" onClick={e => this.toggleActive(art.id)}>Edit</button>
+                                            <button className="delete" onClick={e => this.deleteArticle(art.id)}>Delete</button>
                                         </div>
                                     </header>
                                     <div className="editForm">
+                                        <input type="text" value={art.title}/>
+                                        <textarea value={(!art.text ? [] : typeof(art.text) === 'string' ? [art.text] : art.text).join('\n')}/>
+                                        <section>
+                                            <section>
+                                                <section>
+                                                    <label htmlFor="imgFormat">Image Format: </label>
+                                                    <select id="imgFormat">
+                                                        <option value="wide">Wide</option>
+                                                        <option value="medium">Medium</option>
+                                                        <option value="tall">Tall</option>
+                                                    </select>
+                                                </section>
+                                                <section>
+                                                    <label htmlFor="reversed">Image on the left</label>
+                                                    <input type="checkbox" id="reversed"/>
+                                                </section>
+                                            </section>
+                                        </section>
                                     </div>
                                 </div>
                             ))}
