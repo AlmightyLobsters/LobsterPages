6cc256bf4ca319c1cd29f0cf139f8ae0c4b4cfb3
James Bohacek <jamesbohacek@outlook.com>
2017-02-01 17:33:18 +0100

    upload API in place and working we can now finally publish articles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /* eslint no-console:0 */
 import express from 'express';
+import fs from 'fs';
 import cookieParser from 'cookie-parser';
 import auth from 'basic-auth';
 import bodyParser from 'body-parser';
@@ -97,9 +98,21 @@ app.get('/articles(/:id)?', (req, res) => {
     });
 });
 
-app.post('/articlesTest', requireGroup('ADMIN'), fileUpload(), (req, res) => {
-    console.log('Post made');
-    res.status(200).send('OK');
+app.post('/upload', requireGroup('ADMIN'), fileUpload(), (req, res) => {
+    const file = req.files.file;
+    if(!fs.existsSync(join(publicPath, 'upload'))) fs.mkdirSync(join(publicPath, 'upload'));
+    if(!file) res.status(400).send('File not attached');
+    else {
+        while(fs.existsSync(join(publicPath, 'upload', file.name))) {
+            const splitPattern = /(?:(.+?)(\d+)?)\.(\w{2,4})/i;
+            const [,name, index, ext] = splitPattern.exec(file.name);
+            file.name = `${name}${Number(index) ? Number(index) + 1 : 1}.${ext}`;
+        }
+        fs.writeFile(join(publicPath, 'upload', file.name), file.data, err => {
+            if (err) res.status(500).send(err);
+            else res.status(201).location(`/upload/${file.name}`).send(file);
+        });
+    }
 });
 
 app.post('/articles', bodyParser.json(), requireGroup('ADMIN'), (req, res) => {
