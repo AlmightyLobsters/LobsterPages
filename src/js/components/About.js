e18b197039a71f4fc8e8e9b15ab7b6ff4b3d7707
James Bohacek <jamesbohacek@outlook.com>
2017-02-19 08:18:05 +0100

    Redesign About us
    - About us page made to the same style as Blog page.
    - some css pieces moved from private to public scopes for reusability

--- src/js/components/About.js
+++ src/js/components/About.js
@@ -9,16 +9,18 @@ export const About = _ => (
             </header>
         </section>
         <main>
-            <article>
-                <h3>Co jsme zač</h3>
-                <div className="article_innerWrapper">
-                    <div className="videoWrapper"><iframe width="560" height="349" src="https://www.youtube.com/embed/9YXB2fL1riA" frameBorder="0" allowFullScreen></iframe></div>
-                    <p><em>Jsme skupina 5 studentů 5. ročníku Česko-Francouzské sekce <b>Gymnázia Jana Nerudy</b> v Praze na Malé straně, která se přihlásila do soutěže <a href="http://www.esero.scientica.cz/cansat">CanSat</a>.</em></p>
-                    <p>No, tak to by bylo formální zařazení, a teď po lidsku. Co jsme to za lidi, že jsme dostali ten neobyčejný nápad zabít jakýkoliv zbylý volný čas stavbou čehosi, co snad z povzdálí i připomíná seriózní vědeckou sondu.</p>
-                    <h4>CanSat</h4>
-                    <p>CanSat, název vzniklý sloučením anglického názvu pro plechovku a zkráceným slovem pro satelit. Volně přeloženo tedy plechovkový satelit, <em>satelit v plechovce</em>. Co to je, ptáte se? Jedná se o soutěž ve světě již se 17 letou tradicí. Jak název napovídá, jde o to nacpat co nejvíc technologií, více, či méně, relevantních pro satelity, do pravidly stanovených rozměrů.</p>
-                </div>
-            </article>
+            <section id="content">
+                <article className="articleWrapper">
+                    <h3>Co jsme zač</h3>
+                    <div className="article_innerWrapper">
+                        <div className="videoWrapper"><iframe width="560" height="349" src="https://www.youtube.com/embed/9YXB2fL1riA" frameBorder="0" allowFullScreen></iframe></div>
+                        <p><em>Jsme skupina 5 studentů 5. ročníku Česko-Francouzské sekce <b>Gymnázia Jana Nerudy</b> v Praze na Malé straně, která se přihlásila do soutěže <a href="http://www.esero.scientica.cz/cansat">CanSat</a>.</em></p>
+                        <p>No, tak to by bylo formální zařazení, a teď po lidsku. Co jsme to za lidi, že jsme dostali ten neobyčejný nápad zabít jakýkoliv zbylý volný čas stavbou čehosi, co snad z povzdálí i připomíná seriózní vědeckou sondu.</p>
+                        <h4>CanSat</h4>
+                        <p>CanSat, název vzniklý sloučením anglického názvu pro plechovku a zkráceným slovem pro satelit. Volně přeloženo tedy plechovkový satelit, <em>satelit v plechovce</em>. Co to je, ptáte se? Jedná se o soutěž ve světě již se 17 letou tradicí. Jak název napovídá, jde o to nacpat co nejvíc technologií, více, či méně, relevantních pro satelity, do pravidly stanovených rozměrů.</p>
+                    </div>
+                </article>
+            </section>
         </main>
     </div>
 );
