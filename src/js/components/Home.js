88249677262cd5b9a2d34e60e25f439ba9377354
James Bohacek <jamesbohacek@outlook.com>
2017-03-09 22:03:00 +0100

    Add social tile to HP Add Font Awesome and Space Mono

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -3,21 +3,41 @@ import React from 'react';
 export const Home = _ => (
     <div id="home">
         <section className="hero">
-            <section id="welcome">
+            <section id="welcome" className="v-center">
                 <header>
                     <h1>Vítejte</h1>
                 </header>
             </section>
             <section id="lobsters">
                 <article id="brief" className="col-2-3">
-                    <h2>We are <b>Lobsters</b></h2>
-                    <hr/>
-                    <blockquote>Jsme skupina studentů 5. ročníku <a href="http://gjn.cz">Gymnázia Jana Nerudy</a>. Ačkoliv studujeme ve francouzštině a letos nás čeká francouzská maturita, ve zbytcích volného času se věnujeme moderním technologiím a snažíme se zvýšit veřejné povědomí o problematikách s nimi souvisejícími.</blockquote>
-                    <hr/>
+                    <div id="heading">
+                        <h2>We are <b>Lobsters</b></h2>
+                    </div>
+                    <div id="quote">
+                        <div id="innerQuote">
+                            <hr/>
+                            <blockquote>Jsme skupina studentů 5. ročníku <a href="http://gjn.cz">Gymnázia Jana Nerudy</a>. Ačkoliv studujeme ve francouzštině a letos nás čeká francouzská maturita, ve zbytcích volného času se věnujeme moderním technologiím a snažíme se zvýšit veřejné povědomí o problematikách s nimi souvisejícími.</blockquote>
+                            <hr/>
+                        </div>
+                    </div>
                 </article>
-                <section id="lastArticle" className="col-1-3">
-                    test
-                    {/*some kind of js logic that pulls the last article from the store and puts it here*/}
+                <section id="socialNetworks" className="col-1-3">
+                    <div id="facebook">
+                        ALCanSat
+                        <i className="fa fa-3x fa-facebook-square" aria-hidden="true"></i>
+                    </div>
+                    <div id="twitter">
+                        AlmghtLobsters
+                        <i className="fa fa-3x fa-twitter-square" aria-hidden="true"></i>
+                    </div>
+                    <div id="gplus">
+                        Almighty Lobsters
+                        <i className="fa fa-3x fa-google-plus-square" aria-hidden="true"></i>
+                    </div>
+                    <div id="github">
+                        AlmightyLobsters
+                        <i className="fa fa-3x fa-github-square" aria-hidden></i>
+                    </div>
                 </section>
             </section>
         </section>
