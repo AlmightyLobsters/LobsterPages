b6863719cb4b1a92b017bdbf64ff110b4124bae6
VojtechStep <vojtechstepancik@outlook.com>
2017-03-17 22:51:15 +0100

    Add ability to manually suply auth hash in Authorization header

--- src/js/middleware/auth.js
+++ src/js/middleware/auth.js
@@ -17,13 +17,27 @@ export const authenticate = userGroup => (req, res, next) => {
         req.body.userGroup = userGroup;
     }
     if (!req.body.userGroup) res.status(BAD_REQUEST).send('User group not specified');
-    else if (req.cookies.authHash) res.status(CONFLICT).send('Coookie not secured');
-    else if (!req.signedCookies.authHash) res.status(UNAUTHORIZED).send('Not logged in');
-    else if (!userDB.initialized) res.status(SERVICE_UNAVAILABLE).send('User database not initialized');
-    else userDB.get(req.body.userGroup, (err, result) => {
-        if (err) res.status(INTERNAL_SERVER_ERROR).send(err);
-        else if (!result) res.status(NOT_FOUND).send('User group record not found');
-        else if (result.hashes.includes(req.signedCookies.authHash)) next();
-        else res.status(FORBIDDEN).send('Access denied');
-    });
+    else if (req.get('Authorization')) {
+        const authHeader = req.get('Authorization');
+        if (!authHeader.startsWith('Bearer: ')) crawlCookies();
+        else checkDB(authHeader.substring('Bearer: '.length), true);
+    }
+    else crawlCookies();
+
+    function checkDB (hash, continueWithCookies) {
+        if (!userDB.initialized) res.status(SERVICE_UNAVAILABLE).send('User database not initialized');
+        else userDB.get(req.body.userGroup, (err, result) => {
+            if (err) res.status(INTERNAL_SERVER_ERROR).send(err);
+            else if (!result) res.status(NOT_FOUND).send('User group record not found');
+            else if (result.hashes.includes(hash)) next();
+            else if (continueWithCookies) crawlCookies();
+            else res.status(FORBIDDEN).send('Access denied');
+        });
+    };
+
+    function crawlCookies () {
+        if (req.cookies.authHash) res.status(CONFLICT).send('Coookie not secured');
+        else if (!req.signedCookies.authHash) res.status(UNAUTHORIZED).send('Not logged in');
+        else checkDB(req.signedCookies.authHash, false);
+    };
 };
