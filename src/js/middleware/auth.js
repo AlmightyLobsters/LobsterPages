bdac5f16f7faa69db55f1cb54037c85cc70d1b83
VojtechStep <vojtechstepancik@outlook.com>
2017-03-17 22:07:20 +0100

    ES6-ify

--- src/js/middleware/auth.js
+++ src/js/middleware/auth.js
@@ -11,21 +11,19 @@ userDB.init(err => {
     console.log('Error initializing user database: ', err);
 });
 
-export function authenticate (userGroup) {
-    return (req, res, next) => {
-        if(userGroup !== null) {
-            if (!req.body) req.body = {};
-            req.body.userGroup = userGroup;
-        }
-        if (!req.body.userGroup) res.status(BAD_REQUEST).send('User group not specified');
-        else if (req.cookies.authHash) res.status(CONFLICT).send('Coookie not secured');
-        else if (!req.signedCookies.authHash) res.status(UNAUTHORIZED).send('Not logged in');
-        else if (!userDB.initialized) res.status(SERVICE_UNAVAILABLE).send('User database not initialized');
-        else userDB.get(req.body.userGroup, (err, result) => {
-            if (err) res.status(INTERNAL_SERVER_ERROR).send(err);
-            else if (!result) res.status(NOT_FOUND).send('User group record not found');
-            else if (result.hashes.includes(req.signedCookies.authHash)) next();
-            else res.status(FORBIDDEN).send('Access denied');
-        });
-    };
-}
+export const authenticate = userGroup => (req, res, next) => {
+    if(userGroup !== null) {
+        if (!req.body) req.body = {};
+        req.body.userGroup = userGroup;
+    }
+    if (!req.body.userGroup) res.status(BAD_REQUEST).send('User group not specified');
+    else if (req.cookies.authHash) res.status(CONFLICT).send('Coookie not secured');
+    else if (!req.signedCookies.authHash) res.status(UNAUTHORIZED).send('Not logged in');
+    else if (!userDB.initialized) res.status(SERVICE_UNAVAILABLE).send('User database not initialized');
+    else userDB.get(req.body.userGroup, (err, result) => {
+        if (err) res.status(INTERNAL_SERVER_ERROR).send(err);
+        else if (!result) res.status(NOT_FOUND).send('User group record not found');
+        else if (result.hashes.includes(req.signedCookies.authHash)) next();
+        else res.status(FORBIDDEN).send('Access denied');
+    });
+};
